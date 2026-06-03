00823082f45fe8b6a368ec52cf8c93ccc2a1cbe7
vinyaliu <vinyaliu@tencent.com>
2026-06-03 16:39:54 +0800

    update tree.py

--- tree.py
+++ tree.py
@@ -13,7 +13,17 @@ def tree(dir_path, prefix=''):
             extension = '    ' if is_last else '│   '
             tree(path, prefix + extension)
 
+def start_step():
+        mng_id = OpenApiMngGameFunctionUtils.query_mng_by_name(self, settings.TCMPP_MNG_NAME)
 
+        self.start_step("获取小游戏团队的GlobalTeamId")
+        mini_team_id = OpenApiTeamFunctionUtils.get_team_id(self, settings.TCMPP_MINI_TEAM)
+
+        self.start_step("获取应用团队的GlobalTeamId")
+        team_id = OpenApiTeamFunctionUtils.get_team_id(self, settings.TCMPP_APP_TEAM)
+
+        self.start_step("获取应用id")
+        application_id = OpenApiAppFunctionUtils.get_application_id(self)
 if __name__ == '__main__':
     target = sys.argv[1] if len(sys.argv) > 1 else '.'
     print(target)
